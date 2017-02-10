1bd6d8c53b7cf80da17c063f537a40c985ceb7d6
Jeroen de Bie <Jeroendebie@live.nl>
2017-02-10 15:47:14 +0100

    no message

---
+++ testInThe=Class.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//
+//let presidents = ["USA": "Obama", "Russia": "Putin", "France": "Hollande"]
+//for (country, president) in presidents {
+//    print("The president of France is \(president)")
+//}
+
+let presidents = ["Obama", "Putin", "Hollande"]
+for president in presidents {
+    print[presiden
+}
+
